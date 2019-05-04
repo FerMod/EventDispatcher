582bd973d4696a86f4cec45b935ba7ec5fc35f21
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-04 13:01:13 +0000

    Deploy FerMod/EventDispatcher to github.com/FerMod/EventDispatcher.git:gh-pages

--- deploy_docs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd doxygen
-bash ./generate_docs.sh
--- generate_tree.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-tree --noreport --dirsfirst -I "docs|latex|target|*.sh" -C -H "https://github.com/FerMod/EventDispatcher/tree/master" -T "${PWD##*/} Directory Tree" -o docs/tree.html
