8ca92ca1ff4e499be715024069ec6df36a6258fa
FerMod <tudelaigarcia@gmail.com>
2019-05-04 19:49:30 +0200

    Add missing command

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 
 # Use the current tag version that will be used for doxygen, and to set the maven version
 export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
@@ -9,16 +9,3 @@ mvn --file ../pom.xml clean site
 # Generate doxygen docs
 cd doxygen
 doxygen Doxyfile
-=======
-#!/bin/bash
-set -e # Exit with nonzero exit code if anything fails
-
-# Use the current tag version that will be used for doxygen, and to set the maven version
-export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
-mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
-mvn clean site
-
-# Generate doxygen docs
-cd doxygen
-doxygen Doxyfile
->>>>>>> 8934a7001cc33cf80c69b264cd992cbf70ac8296
