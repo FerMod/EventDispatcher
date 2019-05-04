b4938481642522ce69d950554cabe90112ef4f94
Ferran Tudela <tudelaigarcia@gmail.com>
2019-05-04 19:36:14 +0200

    Update deploy_docs.sh

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
 
 # Use the current tag version that will be used for doxygen, and to set the maven version
 export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
