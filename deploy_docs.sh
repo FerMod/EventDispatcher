ac10825b0baa736c3f0f90e914f25ce18263a158
Ferran Tudela <tudelaigarcia@gmail.com>
2019-05-05 01:04:33 +0200

    Update deploy_docs.sh

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -6,7 +6,7 @@ export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
 mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
 mvn clean site
 
-sudo exec ./generate_tree.sh
+tree --noreport --dirsfirst -I "docs|latex|target|*.sh" -C -H "https://github.com/FerMod/EventDispatcher/tree/master" -T "${PWD##*/} Directory Tree" -o docs/tree.html
 
 # Generate doxygen docs
 cd $TRAVIS_BUILD_DIR/doxygen
