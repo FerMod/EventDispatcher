005dee949f2b619e8054c9212e74ec2f321d5f79
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-04 23:49:04 +0000

    Deploy FerMod/EventDispatcher to github.com/FerMod/EventDispatcher.git:gh-pages

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -6,7 +6,10 @@ export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
 mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
 mvn clean site
 
+#tree --noreport --dirsfirst -I "docs|latex|target|*.sh" -C -H "https://github.com/FerMod/EventDispatcher/tree/master" -T "${PWD##*/} Directory Tree" -o docs/tree.html
+
 # Generate doxygen docs
 cd $TRAVIS_BUILD_DIR/doxygen
 $DOXYGEN_PATH/build/bin/doxygen Doxyfile
+
 cd $TRAVIS_BUILD_DIR
