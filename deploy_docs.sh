8934a7001cc33cf80c69b264cd992cbf70ac8296
Ferran Tudela <tudelaigarcia@gmail.com>
2019-05-04 19:37:27 +0200

    Update deploy_docs.sh

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -3,8 +3,8 @@ set -e # Exit with nonzero exit code if anything fails
 
 # Use the current tag version that will be used for doxygen, and to set the maven version
 export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
-mvn --file ../pom.xml versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
-mvn --file ../pom.xml clean site
+mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
+mvn clean site
 
 # Generate doxygen docs
 cd doxygen
