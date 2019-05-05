48da72cb12bba8b6ff8072a0b542aa0de2843c06
FerMod <tudelaigarcia@gmail.com>
2019-05-05 17:57:05 +0200

    Add docs generation script

---
+++ scripts/generate-deploy-docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails and show script content
+
+cd ..
+
+# Use the current tag version that will be used for doxygen, and to set the maven version
+echo "Updating Maven version from git tag ..."
+export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
+mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
+
+echo "Generating Maven site ..."
+mvn clean site
+
+#tree --noreport --dirsfirst -I "docs|latex|target|*.sh" -C -H "https://github.com/FerMod/EventDispatcher/tree/master" -T "${PWD##*/} Directory Tree" -o docs/tree.html
+
+# Generate doxygen docs
+echo "Generating Doxygen docs ..."
+cd doxygen
+doxygen
+cd ..
+
+git add pom.xml docs/*
+git commit --message "Update docs"
+git push
+
+echo "Deployed master docs ..."
