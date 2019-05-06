b99cb25e778d134097c7df0571c18bfcec2efd62
FerMod <tudelaigarcia@gmail.com>
2019-05-06 21:09:12 +0200

    Change variable name

--- scripts/update-maven-version.sh
+++ scripts/update-maven-version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -ex # Exit with nonzero exit code if anything fails and show script content
-export PROJECT_NUMBER="$(git describe --tags --abbrev=0)"
-mvn versions:set -DnewVersion=${PROJECT_NUMBER#"v"}
+export PROJECT_VERSION="$(git describe --tags --abbrev=0)"
+mvn versions:set -DnewVersion=${PROJECT_VERSION#"v"}
