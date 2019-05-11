a1098e40fca815ebc5aff3cbd874d1a2bc6e5f47
FerMod <tudelaigarcia@gmail.com>
2019-05-11 13:09:59 +0200

    Add script to generate release
    Fixed release and jar name generation
    
    Fixes #22

--- scripts/generate-release.sh
+++ scripts/generate-release.sh
@@ -4,10 +4,14 @@ set -ex # Exit with nonzero exit code if anything fails and show script content
 echo "Generating release ..."
 pushd $(git rev-parse --show-toplevel)
 
+echo "Updating Maven version from git tag ..."
+export PROJECT_VERSION="$(git describe --tags --abbrev=0)"
+mvn versions:set -DnewVersion=${PROJECT_VERSION#"v"}
+
 echo "Generating jar binaries ..."
 mvn clean install
 
-export RELEASE_NAME="$(git describe --tags --abbrev=0)"
+export RELEASE_NAME=$PROJECT_VERSION
 export RELEASE_BODY=""
 printf "Name: %s\nBody: %s\n" $RELEASE_NAME $RELEASE_BODY
 
