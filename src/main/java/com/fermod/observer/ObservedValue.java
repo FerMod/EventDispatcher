c2b8348aeff0ee7c6a734511afdacfeed01c232c
FerMod <tudelaigarcia@gmail.com>
2019-05-20 19:55:52 +0200

    Fix generic type initialization
    This issue could have more and better solutions.
    
    Issue #27

--- src/main/java/com/fermod/observer/ObservedValue.java
+++ src/main/java/com/fermod/observer/ObservedValue.java
@@ -19,13 +19,6 @@ public class ObservedValue<T> extends EventPublisher<ValueChangeListener<T>> imp
 	private T value;
 
 	/**
-	 * Default constructor of the class {@code ObservedValue<T>}. The observed value 
-	 * initialization is leaved to the compiler. This default will be {@code zero} or 
-	 * {@code null}, depending on the data type.
-	 */
-	public ObservedValue() { }
-
-	/**
 	 * Constructor of the class {@code ObservedValue<T>} that takes as parameter the value
 	 * that will be initialized the observed value.
 	 * 
