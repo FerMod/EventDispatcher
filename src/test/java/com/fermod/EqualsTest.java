096ddca821e54423a351917092aae8581aaa2dd3
FerMod <tudelaigarcia@gmail.com>
2019-05-20 20:00:17 +0200

    Increase code coverage
    Closes #24

--- src/test/java/com/fermod/EqualsTest.java
+++ src/test/java/com/fermod/EqualsTest.java
@@ -12,10 +12,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import com.fermod.contract.EqualsContract;
 import com.fermod.extension.TimingExtension;
+import com.fermod.observer.ObservedValue;
 import com.fermod.testdata.serializable.PersonObject;
 import com.fermod.util.RandomString;
 
@@ -28,13 +31,6 @@ class EqualsTest {
 
 	private static final Logger LOGGER = LogManager.getLogger(EqualsTest.class);
 	
-	static RandomString randString;
-	
-	@BeforeAll
-	static void beforeAll() {
-		randString = new RandomString(10, ThreadLocalRandom.current());
-	}
-
 	@AfterAll
 	static void afterAll() {
 	}
@@ -42,10 +38,54 @@ class EqualsTest {
 	@BeforeEach
 	void beforeEach() {
 	}
+	
+	@Nested
+	@DisplayName("ObservedValue Equals Test")
+	class ObservedValueEqualsTest implements EqualsContract<ObservedValue<?>> {
+
+		@Override
+		public ObservedValue<Integer> createValue() {	
+
+			ObservedValue<Integer> observedValue = null;
+			try {
+				observedValue = createObservedValue(10);
+			} catch (Exception e) {
+				fail("Exception raised when creating the test data.", e);
+			}
+
+			return observedValue;
+		}
+
+		@Override
+		public ObservedValue<Integer> createNotEqualValue() {	
+			
+			ObservedValue<Integer> observedValue = null;
+			try {
+				observedValue = createObservedValue(20);
+			} catch (Exception e) {
+				fail("Exception raised when creating the test data.", e);
+			}
+
+			return observedValue;
+		}
+		
+		private ObservedValue<Integer> createObservedValue(int value) {
+			return new ObservedValue<>(value);
+		}
+
+	}
 
 	@Nested
+	@TestInstance(Lifecycle.PER_CLASS)
 	@DisplayName("PersonObject Equals Test")
 	class PersonObjectEqualsTest implements EqualsContract<PersonObject> {
+		
+		RandomString randString;
+		
+		@BeforeAll
+		void beforeAll() {
+			randString = new RandomString(10, ThreadLocalRandom.current());
+		}
 
 		@Override
 		public PersonObject createValue() {	
