ba727af43e2061db1eca3e830cf5fe6acba14ae7
FerMod <tudelaigarcia@gmail.com>
2019-05-22 10:02:01 +0200

    Fix travis config
    Issue #22

--- scripts/generate-release.sh
+++ scripts/generate-release.sh
@@ -11,7 +11,7 @@ mvn versions:set -DnewVersion=${PROJECT_VERSION#"v"}
 echo "Generating jar binaries ..."
 mvn clean install
 
-export RELEASE_NAME="$PROJECT_VERSION"
+export RELEASE_NAME=$PROJECT_VERSION
 export RELEASE_BODY=""
 printf "Name: %s\nBody: %s\n" $RELEASE_NAME $RELEASE_BODY
 
